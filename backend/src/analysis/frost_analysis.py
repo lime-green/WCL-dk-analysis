c80aa0cea07753a01591159744665b9333a0e940
Josh DM <jmarsiglio@gmail.com>
2023-02-26 10:32:14 -0500

    remove ghoul analyzer for frost

--- backend/src/analysis/frost_analysis.py
+++ backend/src/analysis/frost_analysis.py
@@ -361,9 +361,6 @@ class FrostAnalysisScorer(AnalysisScorer):
         consume_score = ScoreWeight(self.get_analyzer(BuffTracker).score(), 1)
         bomb_score = ScoreWeight(self.get_analyzer(BombAnalyzer).score(), 2)
         hyperspeed_score = ScoreWeight(self.get_analyzer(HyperspeedAnalyzer).score(), 1)
-        from .unholy_analysis import GhoulAnalyzer
-
-        print(self.get_analyzer(GhoulAnalyzer).report())
 
         total_score = ScoreWeight.calculate(
             gcd_score,
@@ -391,8 +388,6 @@ class FrostAnalysisConfig(CoreAnalysisConfig):
     show_speed = True
 
     def get_analyzers(self, fight: Fight, buff_tracker_):
-        from .unholy_analysis import GhoulAnalyzer
-
         return super().get_analyzers(fight, buff_tracker_) + [
             DiseaseAnalyzer(fight.encounter.name, fight.duration),
             KMAnalyzer(),
@@ -400,7 +395,6 @@ class FrostAnalysisConfig(CoreAnalysisConfig):
             HowlingBlastAnalyzer(),
             RimeAnalyzer(),
             RaiseDeadAnalyzer(fight.duration),
-            GhoulAnalyzer(fight.duration),
         ]
 
     def get_scorer(self, analyzers):
